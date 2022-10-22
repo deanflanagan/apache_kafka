4d53b7e0845d4951935660f9fe969f77b5845431
spankyf <deanflanagan@gmail.com>
2022-10-22 10:01:07 -0300

    Working on UI testing

--- localInstallation.sh
+++ localInstallation.sh
@@ -1,38 +1,21 @@
 #!/bin/bash
 
+sudo apt-get update
 # install java
 sudo apt install default-jre
 
-# add a kafka user
-sudo useradd -d /opt/kafka -s /bin/bash kafka
-sudo passwd kafka
+# export default java to environment
+echo "JAVA_HOME=/usr/bin/java" | sudo tee -a /etc/environment
 
-# change dir to opt where we'll install
-cd /opt 
-
-# download Kafka
-wget https://dlcdn.apache.org/kafka/3.2.0/kafka_2.13-3.2.0.tgz 
-
-sudo mkdir -p /opt/kafka
-
-# extract compressed kafka and move directory to usr/local
-tar xzf kafka_2.13-3.2.0.tgz -C /opt/kafka --strip-components=1
-sudo chown -R kafka:kafka /opt/kafka
-
-# separate dir for logs
-sudo mkdir /opt/kafka-logs
-sudo chown -R kafka:kafka /opt/kafka-logs
+# download and extract kafka and move directory to /opt
+wget https://downloads.apache.org/kafka/3.3.1/kafka_2.13-3.3.1.tgz
+sudo mkdir /opt/kafka
+sudo tar xzf kafka_2.13-3.3.1.tgz -C /opt/kafka --strip-components=1
+rm kafka_2.13-3.3.1.tgz
 
 # configure kafka and zookeeper as services
-exit
-cd /lib/systemd/system/
-sudo nano zookeeper.service
-
-# paste in zookeeper.service config
-
-# now open and paste in kafka service
-sudo nano kafka.service
-
+sudo cp zookeeper.service /etc/systemd/system/zookeeper.service
+sudo cp kafka.service /etc/systemd/system/kafka.service
 # now reload the systemctl manager and enable and start both services
 
 sudo systemctl daemon-reload 
@@ -43,7 +26,17 @@ sudo systemctl enable zookeeper
 sudo systemctl start kafka
 sudo systemctl enable kafka
 
-# testing phase
-# logging in as the kafka user
+# testing phase. First lets allow all of the 4lw commands for testing
+echo "4lw.commands.whitelist=*" | sudo tee -a /opt/kafka/config/zookeeper.properties 
 
-su - kafka 
+# reload zookeeper service
+sudo systemctl restart zookeeper
+
+# lets download a UI to make life easier
+gh repo clone yahoo/CMAK
+
+# make a build
+./CMAK/sbt clean dist
+
+# one of the last lines will show Your package is ready in /home/dean/Desktop/coding/kakfa_ssl/CMAK/target/universal/cmak-3.0.0.6.zip. Unzip it
+
